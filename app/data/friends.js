c2e1568ee7e905cb0ede11746f323840e179f61e
Latrese <latrese.flowers1979@gmail.com>
2019-08-03 20:57:23 -0700

    errors fixed

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Idris Elba",
+        photo: "https://cbsnews1.cbsistatic.com/hub/i/2018/11/06/0c1af1b8-155a-458e-b105-78f1e7344bf4/2018-11-06t054310z-1334124005-rc1be15a8050-rtrmadp-3-people-sexiest-man.jpg",
+        scores: [5,5,4,1,5,1,2,5,4,5]
+    },
+    {
+        name: "Jennifer Garner",
+        photo: "https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg",
+        scores: [4,3,5,1,2,4,5,2,5,1]
+    },
+    {
+        name: "Selena Gomez",
+        photo: "https://i.ytimg.com/vi/sT261eroZJE/maxresdefault.jpg",
+        scores: [3,3,5,4,2,1,2,5,4,1]
+    },
+    {
+        name: "Mariah Carey",
+        photo: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/celebrities_with_bipolar_slideshow/493ss_getty_rm_mariah_carey.jpg",
+        scores: [2,3,5,4,3,1,5,2,1,4]
+    },
+    {
+        name: "Will Smith",
+        photo: "https://s.hdnux.com/photos/51/23/24/10827008/3/920x920.jpg",
+        scores: [3,5,2,4,3,1,5,2,3,4]
+    },
+    {
+        name: "Warren Buffet",
+        photo: "https://image.cnbcfm.com/api/v1/image/105189303-_Y2A2307.JPG?v=1556890447&w=1400&h=950",
+        scores: [3,1,2,2,1,3,4,5,2,1]
+    },
+    {
+        name: "Mario Lopez",
+        photo: "https://www.dailywire.com/sites/default/files/styles/article_full/public/uploads/2019/07/gettyimages-958155970.jpg?itok=J4qVxcU3",
+        scores: [3,5,4,3,2,1,3,4,5,4]
+    },
+    {
+        name: "Simone Biles",
+        photo: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/celebrities_with_adhd_slideshow/getty_rm_simone_biles.jpg",
+        scores: [5,2,3,4,3,2,1,5,4,3]
+    },
+    {
+        name: "Halle Bailey",
+        photo: "https://www.berkshireeagle.com/uploads/original/20190705-100932-Bailey-T5_81601.jpg",
+        scores: [5,3,4,2,5,1,5,3,4,1]
+    },
+]
+
+module.exports = friends;
